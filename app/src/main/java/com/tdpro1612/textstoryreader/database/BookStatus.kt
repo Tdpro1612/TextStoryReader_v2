d2a79d552f309d716398a0429fe95e22182ac0a7
Tdpro1612 <hangthieulong508@gmail.com>
2026-07-26 09:47:15 +0700

    "add dataclass book status entity"

---
+++ app/src/main/java/com/tdpro1612/textstoryreader/database/BookStatus.kt
@@ -0,0 +1,8 @@
+package com.tdpro1612.textstoryreader.database
+
+enum class BookStatus {
+    UNKNOWN,    // Chưa xác định
+    ONGOING,    // Còn tiếp (Đang ra)
+    PAUSED,     // Tạm dừng
+    COMPLETED   // Hoàn thành / Full
+}
