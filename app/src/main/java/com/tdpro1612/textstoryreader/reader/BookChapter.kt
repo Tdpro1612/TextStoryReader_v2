c4b15bd36dd373fabd10d6a036c9736cd9ee0c5e
Tdpro1612 <hangthieulong508@gmail.com>
2026-08-01 08:34:12 +0700

    "update add path next if 1 chapter in 2 html"

--- app/src/main/java/com/tdpro1612/textstoryreader/reader/BookChapter.kt
+++ app/src/main/java/com/tdpro1612/textstoryreader/reader/BookChapter.kt
@@ -1,8 +1,10 @@
 package com.tdpro1612.textstoryreader.reader
 
 data class BookChapter(
-    val index: Int,              // Thứ tự chương (0, 1, 2...)
-    val title: String,           // Tên chương (Ví dụ: "Chương 1: Mở đầu")
-    val content: String = "",    // Nội dung chữ của chương
-    val startCharOffset: Int = 0 // Vị trí ký tự bắt đầu (dùng cho TXT cắt nhanh)
+    val index: Int,
+    val title: String,
+    val path: String = "",
+    val path_next: String = "",
+    val startCharOffset: Int = 0,
+    val endCharOffset: Int = -1
 )
