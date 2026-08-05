bc534926cb47debefb20418a0afa5137a6775cb3
Tdpro1612 <hangthieulong508@gmail.com>
2026-08-05 09:12:39 +0700

    "delete logic clear all cache after open app"

--- app/src/main/java/com/tdpro1612/textstoryreader/MainActivity.kt
+++ app/src/main/java/com/tdpro1612/textstoryreader/MainActivity.kt
@@ -3,12 +3,15 @@ package com.tdpro1612.textstoryreader
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import com.tdpro1612.textstoryreader.manager.BookCacheManager
 import com.tdpro1612.textstoryreader.ui.library.LibraryActivity
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // 0. Dọn dẹp sạch sẽ tất cả cache tồn đọng của app trước khi vào Thư viện
+//        BookCacheManager.clearAllBookCaches(applicationContext)
 
         // 1. Mở LibraryActivity ngay khi app khởi chạy
         val intent = Intent(this, LibraryActivity::class.java).apply {
