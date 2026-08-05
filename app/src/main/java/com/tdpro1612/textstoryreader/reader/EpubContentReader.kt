f2bc78733435bd8f2115953352fabfdf507145e5
Tdpro1612 <hangthieulong508@gmail.com>
2026-08-05 09:11:20 +0700

    "add log to check time unzip"

--- app/src/main/java/com/tdpro1612/textstoryreader/reader/EpubContentReader.kt
+++ app/src/main/java/com/tdpro1612/textstoryreader/reader/EpubContentReader.kt
@@ -2,12 +2,13 @@ package com.tdpro1612.textstoryreader.reader
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
 import com.tdpro1612.textstoryreader.reader.epub.EpubFileReader
 import com.tdpro1612.textstoryreader.reader.epub.EpubUnzipper
-import com.tdpro1612.textstoryreader.reader.epub.HtmlToTextParser
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.io.File
+import kotlin.system.measureTimeMillis
 
 class EpubContentReader : BookContentReader {
 
@@ -32,19 +33,24 @@ class EpubContentReader : BookContentReader {
      * 1. HÀM GET CHAPTER LIST: Đọc Bảng Mục Lục chương từ thư mục Cache đã Unzip
      */
     override suspend fun getChapterList(context: Context, uri: Uri): List<BookChapter> = withContext(Dispatchers.IO) {
-        val cacheFolder = ensureCacheFolder(context, uri)
+        val cacheFolder: File
+        val ensureCacheTime = measureTimeMillis {
+            cacheFolder = ensureCacheFolder(context, uri)
+        }
+        Log.d("ReaderPerf", "0. [getChapterList] ensureCacheFolder: $ensureCacheTime ms")
+
         if (!cacheFolder.exists()) return@withContext emptyList()
 
         val reader = EpubFileReader(context, cacheFolder)
         return@withContext reader.getChapterList()
     }
 
-    /**
-     * 2. HÀM GET CHAPTER CONTENT: Lấy nội dung chữ của chương qua HtmlToTextParser
-     */
     override suspend fun getChapterContent(context: Context, uri: Uri, chapter: BookChapter): String = withContext(Dispatchers.IO) {
-        // Tự động khôi phục cache nếu bị mất khi tắt app
-        val cacheFolder = ensureCacheFolder(context, uri)
+        val cacheFolder: File
+        val ensureCacheTime = measureTimeMillis {
+            cacheFolder = ensureCacheFolder(context, uri)
+        }
+        Log.d("ReaderPerf", "0. [getChapterContent] ensureCacheFolder: $ensureCacheTime ms")
 
         if (!cacheFolder.exists()) {
             return@withContext "Không thể giải nén hoặc đọc nội dung sách."
@@ -52,5 +58,17 @@ class EpubContentReader : BookContentReader {
 
         val content = HtmlToTextParser.parseChapter(chapter, cacheFolder)
         return@withContext if (content.isNotBlank()) content else "Nội dung chương rỗng."
+    }
+
+    /**
+     * 3. HÀM clear cache folder
+     */
+    override fun clearCache(context: Context, uri: Uri) {
+        // Tự tính toán lại vị trí folder cache theo URI
+        val folderName = "epub_cache_" + uri.toString().hashCode()
+        val cacheFolder = File(context.cacheDir, folderName)
+
+        // 🔥 Gọi xóa thư mục cache của cuốn sách này
+        EpubUnzipper.clearCache(cacheFolder)
     }
 }
