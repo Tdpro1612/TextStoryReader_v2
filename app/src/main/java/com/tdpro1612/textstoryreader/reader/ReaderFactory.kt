b85df3f35183f5b8617a0be89bcdb075c75ff012
Tdpro1612 <hangthieulong508@gmail.com>
2026-08-05 09:14:51 +0700

    "delete PRC to unsupport"

--- app/src/main/java/com/tdpro1612/textstoryreader/reader/ReaderFactory.kt
+++ app/src/main/java/com/tdpro1612/textstoryreader/reader/ReaderFactory.kt
@@ -8,8 +8,6 @@ object ReaderFactory {
 
     private val epubReader by lazy { EpubContentReader() }
 
-    private val prcReader by lazy { PrcContentReader() }
-
     fun getReader(uri: Uri): BookContentReader {
 
         val fileName = (uri.lastPathSegment ?: uri.path ?: "")
@@ -22,10 +20,6 @@ object ReaderFactory {
 
             fileName.endsWith(".epub") ->
                 epubReader
-
-            fileName.endsWith(".prc") ||
-            fileName.endsWith(".mobi") ->
-                prcReader
 
             else ->
                 throw UnsupportedOperationException(
--- app/src/main/java/com/tdpro1612/textstoryreader/scanner/BookParserFactory.kt
+++ app/src/main/java/com/tdpro1612/textstoryreader/scanner/BookParserFactory.kt
@@ -3,10 +3,10 @@ package com.tdpro1612.textstoryreader.scanner
 object BookParserFactory {
 
     // Tập hợp các đuôi file được ứng dụng hỗ trợ
-    private val SUPPORTED_EXTENSIONS = setOf("EPUB", "TXT", "PRC")
+    private val SUPPORTED_EXTENSIONS = setOf("EPUB", "TXT")
 
     /**
-     * Trả về tập hợp tất cả đuôi file được hỗ trợ (.EPUB, .TXT, .PRC)
+     * Trả về tập hợp tất cả đuôi file được hỗ trợ (.EPUB, .TXT)
      */
     fun getSupportedExtensions(): Set<String> {
         return SUPPORTED_EXTENSIONS
